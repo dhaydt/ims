9704e7794acafef3fd18755652d9de0c6a5d42a0
dhaydt <1992dayat@gmail.com>
2020-08-08 18:48:12 +0700

    Updates

---
+++ precache-manifest.8bd2207a2c1649e11929a3d6eaa5e484.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e48719a609a90ea6b8a333c7c962fb",
+    "url": "/ims/index.html"
+  },
+  {
+    "revision": "905f6e625186b834b5a8",
+    "url": "/ims/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "e2d3549322cc5d65e74d",
+    "url": "/ims/static/css/main.7c0ff9fd.chunk.css"
+  },
+  {
+    "revision": "905f6e625186b834b5a8",
+    "url": "/ims/static/js/2.35f9d5aa.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ims/static/js/2.35f9d5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d3549322cc5d65e74d",
+    "url": "/ims/static/js/main.3e3ebd03.chunk.js"
+  },
+  {
+    "revision": "24b5f8d5e6fbd133b49e",
+    "url": "/ims/static/js/runtime-main.2b793064.js"
+  },
+  {
+    "revision": "e4093059547b462e2136e7c496a533c6",
+    "url": "/ims/static/media/appdt.e4093059.png"
+  },
+  {
+    "revision": "6dab99b67d4b0326df6de784959c62e3",
+    "url": "/ims/static/media/appmb.6dab99b6.png"
+  },
+  {
+    "revision": "9c8ee4f8d1efcb2e3b26b77663727fe3",
+    "url": "/ims/static/media/design.9c8ee4f8.png"
+  },
+  {
+    "revision": "3514e38783e121c388bf0e3ba50e8d9a",
+    "url": "/ims/static/media/ecmr.3514e387.png"
+  },
+  {
+    "revision": "90cddf3f9f310149b723400bc5089c21",
+    "url": "/ims/static/media/game.90cddf3f.png"
+  },
+  {
+    "revision": "2002c61e5e770bdcd5e132757deabe73",
+    "url": "/ims/static/media/img.2002c61e.svg"
+  },
+  {
+    "revision": "24c36218b2fef8ca438cad39c79f4f13",
+    "url": "/ims/static/media/img2.24c36218.svg"
+  },
+  {
+    "revision": "3372f60935dff6d4162f8d3c22b0b539",
+    "url": "/ims/static/media/s1.3372f609.jpg"
+  }
+]);
